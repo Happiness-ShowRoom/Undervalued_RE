a7dd7021ee81cee91492cb3e7153fe8736bf293f
Happiness-ShowRoom <45061612+Happiness-ShowRoom@users.noreply.github.com>
2018-11-15 03:49:54 -0500

    Update conf.js

--- Configuration/conf.js
+++ Configuration/conf.js
@@ -11,7 +11,7 @@ exports.config = {
         'browserName': 'chrome',
         'chromeOptions': {
 
-            // args: [ "--headless", "--disable-gpu", "--window-size=800,600" ],
+            args: [ "--headless", "--disable-gpu", "--window-size=800,600" ],
 
             prefs: {
                 download: {
